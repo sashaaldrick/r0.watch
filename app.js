48b04e2b4a61be91780ca1755a9e2357511716ea
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:04:17 +0000

    format list from JSON, add basic styling

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 async function init() {
   const resp = await fetch('/api/github')
-  const data = await resp.json()
-  document.getElementById('output').textContent = JSON.stringify(data, null, 2)
+  const events = await resp.json()
+  
+  const output = document.getElementById('output')
+  output.innerHTML = events.map(event => `
+    <div class="event">
+      <b>${event.type}</b>: ${event.actor.login} - ${new Date(event.created_at).toLocaleString()}
+    </div>
+  `).join('')
 }
 
 init()
