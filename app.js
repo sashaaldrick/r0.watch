e8d50235e8c85681778aae2cc5ea72d180437a92
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:39:04 +0000

    add loading and PR links

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 async function init() {
+  const output = document.getElementById('output')
+  output.innerHTML = '<div class="loading">Loading...</div>'
+  
   const resp = await fetch('/api/github')
   const prs = await resp.json()
   
-  const output = document.getElementById('output')
   output.innerHTML = prs
     .filter(pr => pr.merged_at)
     .map(pr => `
       <div class="event">
-        <b>Merged PR #${pr.number}</b>: ${pr.user.login} - ${new Date(pr.merged_at).toLocaleString()}
+        <b>Merged PR <a href="${pr.html_url}" target="_blank">#${pr.number}</a></b>: 
+        <a href="https://github.com/${pr.user.login}" target="_blank">${pr.user.login}</a> 
+        - ${new Date(pr.merged_at).toLocaleString()}
         <div class="title">${pr.title}</div>
         ${pr.body ? `<div class="description">${pr.body}</div>` : ''}
       </div>
