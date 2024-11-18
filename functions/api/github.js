739403aef520464e2112187c065724c138fe152e
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:15:59 +0000

    add state=closed

--- functions/api/github.js
+++ functions/api/github.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  // get merged PRs, 100 per page, sorted by updated date
+  // add state=closed to get merged PRs
   const response = await fetch(
     'https://api.github.com/repos/risc0/risc0/pulls?state=closed&sort=updated&direction=desc&per_page=100',
     {
