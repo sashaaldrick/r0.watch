965633702f72c7a076b32eabd555b9bed1493711
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:09:53 +0000

    use gh pulls API

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 async function init() {
   const resp = await fetch('/api/github')
-  const events = await resp.json()
+  const prs = await resp.json()
   
   const output = document.getElementById('output')
-  output.innerHTML = events
-    // filter only merged PRs
-    .filter(event => 
-      event.type === 'PullRequestEvent' && 
-      event.payload.pull_request.merged
-    )
-    .map(event => {
-      const pr = event.payload.pull_request
-      return `
-        <div class="event">
-          <b>Merged PR #${pr.number}</b>: ${event.actor.login} - ${new Date(event.created_at).toLocaleString()}
-          <div class="details">${pr.title}</div>
-        </div>
-      `
-    }).join('')
+  output.innerHTML = prs
+    .filter(pr => pr.merged)
+    .map(pr => `
+      <div class="event">
+        <b>Merged PR #${pr.number}</b>: ${pr.user.login} - ${new Date(pr.merged_at).toLocaleString()}
+        <div class="details">${pr.title}</div>
+      </div>
+    `).join('')
 }
 
 init()
--- functions/api/github.js
+++ functions/api/github.js
@@ -1,6 +1,7 @@
 export async function onRequest(context) {
+  // get merged PRs, 100 per page, sorted by updated date
   const response = await fetch(
-    'https://api.github.com/repos/risc0/risc0/events',
+    'https://api.github.com/repos/risc0/risc0/pulls?state=closed&sort=updated&direction=desc&per_page=100',
     {
       headers: {
         'Authorization': `token ${context.env.GITHUB_TOKEN}`,
