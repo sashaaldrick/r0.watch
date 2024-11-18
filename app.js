329961cf25827bbdc7c231ad7d561bf9521bcc04
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:34:15 +0000

    add body from PRs and a tiny bit extra styling

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ async function init() {
   const resp = await fetch('/api/github')
   const prs = await resp.json()
   
-  console.log('total prs:', prs.length)
-  console.log('merged prs:', prs.filter(pr => pr.merged_at).length)  // use merged_at instead of merged
-  
   const output = document.getElementById('output')
   output.innerHTML = prs
-    .filter(pr => pr.merged_at)  // merged PRs have a merged_at timestamp
+    .filter(pr => pr.merged_at)
     .map(pr => `
       <div class="event">
         <b>Merged PR #${pr.number}</b>: ${pr.user.login} - ${new Date(pr.merged_at).toLocaleString()}
-        <div class="details">${pr.title}</div>
+        <div class="title">${pr.title}</div>
+        ${pr.body ? `<div class="description">${pr.body}</div>` : ''}
       </div>
     `).join('')
 }
