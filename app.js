1dbf45eb24fd00be8937126868e9a0a4a684f91e
Sasha Aldrick <sasha@aldrick.dev>
2024-11-18 16:07:59 +0000

    show only merged PRs

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ async function init() {
   const events = await resp.json()
   
   const output = document.getElementById('output')
-  output.innerHTML = events.map(event => `
-    <div class="event">
-      <b>${event.type}</b>: ${event.actor.login} - ${new Date(event.created_at).toLocaleString()}
-    </div>
-  `).join('')
+  output.innerHTML = events
+    // filter only merged PRs
+    .filter(event => 
+      event.type === 'PullRequestEvent' && 
+      event.payload.pull_request.merged
+    )
+    .map(event => {
+      const pr = event.payload.pull_request
+      return `
+        <div class="event">
+          <b>Merged PR #${pr.number}</b>: ${event.actor.login} - ${new Date(event.created_at).toLocaleString()}
+          <div class="details">${pr.title}</div>
+        </div>
+      `
+    }).join('')
 }
 
 init()
